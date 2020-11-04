9b2ce0a15160661417148e4f17e6eab583c4acdf
Sarah Gibson <sgibson@turing.ac.uk>
2020-11-04 13:23:00 +0000

    🚧 Deploy Kubernetes cluster and node pool in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,37 @@ provider "google" {
     project     = var.project_id
     region      = var.region
 }
+
+# Kubernetes cluster
+resource "google_container_cluster" "primary" {
+    name                     = var.cluster_name
+    location                 = var.zone
+    remove_default_node_pool = true
+    initial_node_count       = 1
+}
+
+# Kubernetes default node
+resource "google_container_node_pool" "primary_node_pool" {
+    name       = "${google_container_cluster.primary.name}-default"
+    location   = var.zone
+    cluster    = google_container_cluster.primary.name
+    node_count = var.node_count
+
+    node_config {
+        preemptible  = true
+        machine_type = var.machine_type
+
+        metadata = {
+            disable-legacy-endpoints = "true"
+        }
+
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+        ]
+    }
+
+    management {
+        auto_upgrade = true
+    }
+}
