cc9c71aba94536d1584d8b676882311ae6253151
Sarah Gibson <sgibson@turing.ac.uk>
2020-11-04 11:55:50 +0000

    🚧 Update terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,14 @@
+variable "credentials_file" {
+    type        = string
+    description = "Path to Google Service Account key in JSON format"
+}
+
 variable "project_id" {
     type        = string
     description = "The Google Project ID to deploy the BinderHub into"
 }
 
-variable "credentials_file" {
-    type        = string
-    description = "Path to Google Service Account key in JSON format"
+variable "region" {
+    type = string
+    description = "The region within which to deploy the BinderHub"
 }
