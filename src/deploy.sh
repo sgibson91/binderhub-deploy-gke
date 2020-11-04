d716413550e7ac8900bb2c70b6359ea823b5aad3
Sarah Gibson <sgibson@turing.ac.uk>
2020-11-04 17:01:39 +0000

    🚧 Add BinderHub and Docker variables and check them

--- src/deploy.sh
+++ src/deploy.sh
@@ -11,25 +11,32 @@ configFile="${DIR}/config.json"
 
 echo "--> Reading configuration from ${configFile}"
 
+BINDERHUB_NAME=$(jq -r '.binderhub .name' "${configFile}")
+BINDERHUB_VERSION=$(jq -r '.binderhub .version' "${configFile}")
+DOCKER_ORG=$(jq -r '.docker .org' "${configFile}")
+DOCKER_USERNAME=$(jq -r '.docker .username' "${configFile}")
+DOCKER_PASSWORD=$(jq -r '.docker .password' "${configFile}")
 GCP_ACCOUNT_EMAIL=$(jq -r '.gcp .email_account' "${configFile}")
 GCP_PROJECT_ID=$(jq -r '.gcp .project_id' "${configFile}")
 GCP_PROJECT_CREDS=$(jq -r '.gcp .credentials_file' "${configFile}")
 GCP_REGION=$(jq -r '.gcp .region' "${configFile}")
 GCP_ZONE=$(jq -r '.gcp .zone' "${configFile}")
-GKE_CLUSTER_NAME=$(jq -r '.gke .cluster_name' "${configFile}")
 GKE_NODE_COUNT=$(jq -r '.gke .node_count' "${configFile}")
 GKE_MACHINE_TYPE=$(jq -r '.gke .machine_type' "${configFile}")
+IMAGE_PREFIX=$(jq -r '.binderhub .image_prefix' "${configFile}")
 
 # Check that all variables are set non-zero, non-null
 REQUIRED_VARS=" \
+  BINDERHUB_NAME \
+  BINDERHUB_VERSION \
   GCP_ACCOUNT_EMAIL \
   GCP_PROJECT_ID \
   GCP_PROJECT_CREDS \
   GCP_REGION \
   GCP_ZONE \
-  GKE_CLUSTER_NAME \
   GKE_NODE_COUNT \
   GKE_MACHINE_TYPE \
+  IMAGE_PREFIX \
   "
 for required_var in $REQUIRED_VARS ; do
   if [ -z "${!required_var}" ] || [ "x${!required_var}" == 'xnull' ] ; then
@@ -38,13 +45,45 @@ for required_var in $REQUIRED_VARS ; do
   fi
 done
 
+# Generate valid name for GKE cluster
+GKE_CLUSTER_NAME=$(echo "${BINDERHUB_NAME}" | tr -cd '[:alnum:]-' | tr '[:upper:]' '[:lower:]' | cut -c 1-59)
+GKE_CLUSTER_NAME="${GKE_CLUSTER_NAME}-gke"
+
+# Check if any optional variables are set null; if so, reset them to a
+# zero-length string for later checks. If they failed to read at all,
+# possibly due to an invalid JSON file, they will be returned as a
+# zero-length string -- this is attempting to make the 'not set'
+# value the same in either case.
+if [ "x${DOCKER_ORG}" == 'xnull' ] ; then DOCKER_ORG='' ; fi
+if [ "x${DOCKER_USERNAME}" == 'xnull' ] ; then DOCKER_USERNAME='' ; fi
+if [ "x${DOCKER_PASSWORD}" == 'xnull' ] ; then DOCKER_PASSWORD='' ; fi
+
 # Normalise region, zone and machine_type to remove spaces and have lowercase
 GCP_REGION=$(echo "${GCP_REGION//[[:blank:]]/}" | tr "[:upper:]" "[:lower:]")
 GCP_ZONE=$(echo "${GCP_ZONE//[[:blank:]]/}" | tr "[:upper:]" "[:lower:]")
 GKE_MACHINE_TYPE=$(echo "${GKE_MACHINE_TYPE//[[:blank:]]/}" | tr "[:upper:]" "[:lower:]")
 
+# Check/get user's Docker credentials
+if [ -z "${DOCKER_USERNAME}" ] ; then
+  if [ -n "${DOCKER_ORG}" ] ; then
+    echo "--> Your Docker ID must be a member of the ${DOCKER_ORG} organisation"
+  fi
+  read -rp "Docker Hub ID: " DOCKER_USERNAME
+  read -rsp "Docker Hub password: " DOCKER_PASSWORD
+  echo
+else
+  if [ -z "${DOCKER_PASSWORD}" ] ; then
+    read -rsp "Docker Hub password for ${DOCKER_USERNAME}: " DOCKER_PASSWORD
+    echo
+  fi
+fi
+
 # Print configuration
 echo "--> Configuration to deploy:
+  BINDERHUB_NAME: ${BINDERHUB_NAME}
+  BINDERHUB_VERSION: ${BINDERHUB_VERSION}
+  DOCKER_ORG: ${DOCKER_ORG}
+  DOCKER_USERNAME: ${DOCKER_USERNAME}
   GCP_ACCOUNT_EMAIL: ${GCP_ACCOUNT_EMAIL}
   GCP_PROJECT_ID: ${GCP_PROJECT_ID}
   GCP_PROJECT_CREDS: ${GCP_PROJECT_CREDS}
@@ -53,6 +92,7 @@ echo "--> Configuration to deploy:
   GKE_CLUSTER_NAME: ${GKE_CLUSTER_NAME}
   GKE_MACHINE_TYPE: ${GKE_MACHINE_TYPE}
   GKE_NODE_COUNT: ${GKE_NODE_COUNT}
+  IMAGE_PREFIX: ${IMAGE_PREFIX}
   " | tee "${DIR}"/read-config.log
 
 # Deploy cluster using terraform
@@ -93,7 +133,7 @@ kubectl create clusterrolebinding cluster-admin-binding \
 
 # Check helm installation
 helm=$(command -v helm3 || command -v helm)
-HELM_VERSION=$($helm version --short | cut -f1 -d".")
+HELM_VERSION=$($helm version -c --short | cut -f1 -d".")
 
 if [ "${HELM_VERSION}" == "v3" ] ; then
   echo "--> You are running helm v3!"
@@ -106,5 +146,6 @@ else
   exit 1
 fi
 
+# Setup helm repositories
 $helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
 $helm repo update
