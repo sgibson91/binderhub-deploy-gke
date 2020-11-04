b346a6be3fbd5cd017cc4237d39a54c61584dec4
Sarah Gibson <sgibson@turing.ac.uk>
2020-11-04 13:55:58 +0000

    🚧 Add and connect vnet/subnet to k8s cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,8 +19,12 @@ provider "google" {
 resource "google_container_cluster" "primary" {
     name                     = var.cluster_name
     location                 = var.zone
+
     remove_default_node_pool = true
     initial_node_count       = 1
+
+    network                  = google_compute_network.vpc.name
+    subnetwork               = google_compute_subnetwork.subnet.name
 }
 
 # Kubernetes default node
