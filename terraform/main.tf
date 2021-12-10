82eef973733341b229d0aaf50462b82b00b3f6ff
Sarah Gibson <drsarahlgibson@gmail.com>
2021-12-10 20:30:07 +0000

    Update terraform google provider version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
 terraform {
-    required_version = ">= 0.12"
+    required_version = ">= 0.13"
 
     required_providers {
         google = {
             source  = "hashicorp/google"
+            version = "4.3.0"
         }
     }
 }
 
 provider "google" {
-    version     = "3.46.0"
     credentials = file(var.credentials_file)
     project     = var.project_id
     region      = var.region
